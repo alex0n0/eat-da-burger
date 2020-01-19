06c12ee1a420cf860095f920c283db896af0e8fc
aaa_alex <alexwu7583@gmail.com>
2020-01-19 17:45:27 +1100

    update README

--- config/connection.js
+++ config/connection.js
@@ -1,27 +1,3 @@
-// var mysql = require("mysql");
-// var keys = require('./keys.js');
-
-// var connection = mysql.createConnection({
-//   host: keys.DB_HOST,
-//   port: 3306,
-//   user: keys.DB_USER,
-//   password: keys.DB_PASSWORD,
-//   database: keys.DB_DATABASE
-// });
-
-// // Make connection.
-// connection.connect(function(err) {
-//   if (err) {
-//     console.error("error connecting: " + err.stack);
-//     return;
-//   }
-//   console.log("connected as id " + connection.threadId);
-// });
-
-// // Export connection for our ORM to use.
-// module.exports = connection;
-
-
 var mysql = require("mysql");
 var keys = require('./keys.js');
 
